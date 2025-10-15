2a915e96c115e2861f39604ffd1e1429f19edff3
Himans <himansbhardwaj1566@gmail.com>
2025-10-15 19:51:01 +0530

    add project

---
+++ client/src/components/home/Banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="w-full py-2.5 font-medium text-sm text-indigo-800 text-center bg-gradient-to-r from-[#7e8dff] to-[#FDFEFF]">
+      <p>
+        <span className="px-3 py-1 rounded-lg text-white bg-indigo-600 mr-2">
+          New
+        </span>
+        AI Resume Builder
+      </p>
+    </div>
+  );
+};
+
+export default Banner;

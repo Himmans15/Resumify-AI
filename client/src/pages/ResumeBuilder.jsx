02955148265c79feac60bbad708e00890c9d4bbf
Himans <himansbhardwaj1566@gmail.com>
2025-10-25 14:58:42 +0530

    add files

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import { ArrowLeftIcon } from "lucide-react";
 
 const ResumeBuilder = () => {
-  return <div>ResumeBuilder</div>;
+  const { resumeId } = useParams();
+
+  const [resumeData, setResumeData] = useState({
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+  });
+
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find((resume) => resume._id === resumeId);
+    if (resume) {
+      setResumeData(resume);
+      document.title = resume.title;
+    }
+  };
+
+  useEffect(() => {
+    loadExistingResume();
+  }, []);
+
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 py-6 ">
+        <Link to={"/app"}>
+          <ArrowLeftIcon className="size-4" />
+          Back to Dashboard
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeBuilder;

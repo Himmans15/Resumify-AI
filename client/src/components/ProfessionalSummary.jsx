9370ec3ac2ee7a60e683931f3d64890c66ab8522
Himans <himansbhardwaj1566@gmail.com>
2025-10-29 19:32:43 +0530

    update files

--- client/src/components/ProfessionalSummary.jsx
+++ client/src/components/ProfessionalSummary.jsx
@@ -1,3 +1,4 @@
+import { Sparkles } from "lucide-react";
 import React from "react";
 
 const ProfessionalSummary = ({ data, onChange, setResumeData }) => {
@@ -8,8 +9,21 @@ const ProfessionalSummary = ({ data, onChange, setResumeData }) => {
           <h3 className="flex items-center gap-2 text-lg font-semibold text-gray-900">
             Professional Summary
           </h3>
+          <p className="text-sm text-gray-500">
+            Add summary for your resume here
+          </p>
         </div>
-        <button></button>
+        <button className="flex items-center gap-2 px-3 py-1 text-sm bg-purple-100 text-purple-700 rounded hover:bg-purple-200 transition-colors disabled:opacity-50">
+          <Sparkles className="size-4" /> AI Enhance
+        </button>
+      </div>
+      <div className="mt-6">
+        <textarea
+          name=""
+          id=""
+          className="w-full p-3 px-4 mt-2 border text-sm border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors resize-none"
+          placeholder="Write a compelling professional summary that highlights your key strengths and career objectives... "
+        />
       </div>
     </div>
   );

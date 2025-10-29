4df65951aa333f9c0c287e21a22756cfd57d686e
Himans <himansbhardwaj1566@gmail.com>
2025-10-29 12:48:43 +0530

    add files

---
+++ client/src/components/ProfessionalSummary.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ProfessionalSummary = ({ data, onChange, setResumeData }) => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <div>
+          <h3 className="flex items-center gap-2 text-lg font-semibold text-gray-900">
+            Professional Summary
+          </h3>
+        </div>
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default ProfessionalSummary;

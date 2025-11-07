483d3312a3f4b87e73ea11081d7180f1cc47d167
Himans <himansbhardwaj1566@gmail.com>
2025-11-07 14:01:14 +0530

    add more files

---
+++ server/controllers/resumeController.js
@@ -0,0 +1,35 @@
+import Resume from "../models/Resume.js";
+
+// controller for creating a new resume
+// POST: /api/resumes/create
+export const createResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { title } = req.body;
+
+    // create new resume
+    const newResume = await Resume.create({ userId, title });
+    // return success message
+    return res
+      .status(201)
+      .json({ message: "Resume created successfully", resume: newResume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for delete resume
+// DELETE: /api/resumes/delete
+export const deleteResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { resumeId } = req.params;
+
+    await Resume.findOneAndDelete({ userId, _id: resumeId });
+
+    // return success message
+    return res.status(200).json({ message: "Resume deleted successfully" });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};

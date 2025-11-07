6ab9ee2d0bebb0f68274379a78269ecbf4c630ec
Himans <himansbhardwaj1566@gmail.com>
2025-11-07 12:09:31 +0530

    adding middlewares

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -74,3 +74,26 @@ export const loginUser = async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 };
+
+// controller for getting user by id
+// GET: /api/user/data
+
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    // check if user exists
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // return user
+    user.password = undefined;
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};

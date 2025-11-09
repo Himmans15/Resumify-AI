1ef49bb6f14ec044b4b7e42ab8e6fe2061e53c50
Himans <himansbhardwaj1566@gmail.com>
2025-11-09 12:40:54 +0530

    resume router and imagekit funciton

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,4 +1,6 @@
+import imagekit from "../configs/imagekit.js";
 import Resume from "../models/Resume.js";
+import fs from "fs";
 
 // controller for creating a new resume
 // POST: /api/resumes/create
@@ -87,6 +89,22 @@ export const updateResume = async (req, res) => {
     const image = req.file;
 
     let resumeDataCopy = JSON.parse(resumeData);
+
+    if (image) {
+      const imageBufferData = fs.createReadStream(image.path);
+
+      const response = await imagekit.files.upload({
+        file: imageBufferData,
+        fileName: "resume.png",
+        folder: "user-resume",
+        transformation: {
+          pre:
+            "w-300,h-300,fo-face,z-0.75" +
+            (removeBackground ? ",e-bgremove" : ""),
+        },
+      });
+      resumeDataCopy.personal_info.image = response.url;
+    }
 
     const resume = await Resume.findByIdAndUpdate(
       { userId, _id: resumeId },

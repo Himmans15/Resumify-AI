c6938d30f832332c8523baa74696d4d4ad9d8115
Himans <himansbhardwaj1566@gmail.com>
2025-11-21 13:38:20 +0530

    auth user login frontend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -62,7 +62,7 @@ export const loginUser = async (req, res) => {
     }
 
     // check if password is correct or not
-    if (!user.comparepassword(password)) {
+    if (!user.comparePassword(password)) {
       return res.status(400).json({ message: "Invalid email or password" });
     }
 
